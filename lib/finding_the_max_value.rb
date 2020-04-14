4cd60e9d7bff835dfacb06850b230dc0497fd18d
ILJOO CHAE <9662964@gmail.com>
2020-04-14 04:01:23 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,12 @@
 def find_max_value(array)
-  # Add your solution here
-end
+    # Add your solution here
+    counter = 0
+    maxValue = 0
+    while counter < array.length do
+      if maxValue < array[counter]
+        maxValue = array[counter]
+      end
+      counter+=1
+    end
+    return maxValue  
+  end
